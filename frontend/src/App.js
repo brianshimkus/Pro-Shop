dbf7a7d629fd0d4fde9ecce0c56a43e45dbc0d20
Brian Shimkus <brianshimkus1@gmail.com>
2023-05-11 00:06:06 -0500

    Created Header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,15 @@
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+
 export default function App() {
-	return <div>Welcome to Pro Shop</div>
+	return (
+		<>
+			<Header />
+			<main className='py-3'>
+				<Container>
+					<h1>Welcome to Pro Shop</h1>
+				</Container>
+			</main>
+		</>
+	)
 }
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,25 @@
+import { Navbar, Nav, Container, NavDropdown, Badge } from 'react-bootstrap'
+import { FaShoppingCart, FaUser } from 'react-icons/fa'
+
+export default function Header() {
+	return (
+		<header>
+			<Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+				<Container>
+					<Navbar.Brand>ProShop</Navbar.Brand>
+					<Navbar.Toggle aria-controls='basic-navbar-nav' />
+					<Navbar.Collapse id='basic-navbar-nav'>
+						<Nav className='ml-auto'>
+							<Nav.Link href='/cart'>
+								<FaShoppingCart /> Cart
+							</Nav.Link>
+							<Nav.Link href='/login'>
+								<FaUser /> Sign In
+							</Nav.Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</header>
+	)
+}
