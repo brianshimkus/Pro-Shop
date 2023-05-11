07c5c8747f8f9f45ff9a60f2975ea23b75839fb2
Brian Shimkus <brianshimkus1@gmail.com>
2023-05-11 00:08:17 -0500

    Created Footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 export default function App() {
 	return (
@@ -10,6 +11,7 @@ export default function App() {
 					<h1>Welcome to Pro Shop</h1>
 				</Container>
 			</main>
+			<Footer />
 		</>
 	)
 }
