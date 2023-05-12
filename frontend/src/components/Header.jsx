1fc84933f518e0119440120aa232fd165af529b4
Brian Shimkus <brianshimkus1@gmail.com>
2023-05-11 19:18:15 -0500

    Added logo

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,15 +1,18 @@
 import { Navbar, Nav, Container, NavDropdown, Badge } from 'react-bootstrap'
 import { FaShoppingCart, FaUser } from 'react-icons/fa'
+import logo from '../assets/logo.png'
 
 export default function Header() {
 	return (
 		<header>
 			<Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
 				<Container>
-					<Navbar.Brand>ProShop</Navbar.Brand>
+					<Navbar.Brand href='/'>
+						<img src={logo} alt='Pro Shop' /> ProShop
+					</Navbar.Brand>
 					<Navbar.Toggle aria-controls='basic-navbar-nav' />
 					<Navbar.Collapse id='basic-navbar-nav'>
-						<Nav className='ml-auto'>
+						<Nav className='ms-auto'>
 							<Nav.Link href='/cart'>
 								<FaShoppingCart /> Cart
 							</Nav.Link>
