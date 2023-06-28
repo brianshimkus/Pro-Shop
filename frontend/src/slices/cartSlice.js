f01a5475245eb1973514bdabd33df6dcf88081ab
Brian Shimkus <brianshimkus1@gmail.com>
2023-06-27 23:56:01 -0500

    Calculate items price

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -4,6 +4,10 @@ const initialState = localStorage.getItem('cart')
 	? JSON.parse(localStorage.getItem('cart'))
 	: { cartItems: [] }
 
+const addDecimals = (num) => {
+	return (Math.round(num * 100) / 100).toFixed(2)
+}
+
 const cartSlice = createSlice({
 	name: 'cart',
 	initialState,
@@ -22,6 +26,10 @@ const cartSlice = createSlice({
 			}
 
 			// Calculate items price
+			state.itemsPrice = addDecimals(
+				state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+			)
+
 			// Calculate shipping price
 			// Calculate tax price
 			// Calculate total price
