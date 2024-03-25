02832adbac799e83de4c910f38230e1bea73145b
Brian Shimkus <brianshimkus1@gmail.com>
2024-03-25 15:13:10 -0500

    Built header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,17 @@
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 export default function App() {
-	return <div>Pro Shop</div>
+	return (
+		<>
+			<Header />
+			<main className='py-3'>
+				<Container>
+					<h1>Pro Shop</h1>
+				</Container>
+			</main>
+			<Footer />
+		</>
+	)
 }
