1d8eb87f00e4f78f701f94fde4519fc31b040d1b
Brian Shimkus <brianshimkus1@gmail.com>
2024-03-27 15:54:19 -0500

    Product API slice and then get product details

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,78 +1,83 @@
 import { Link, useParams } from 'react-router-dom'
-import axios from 'axios'
 import { Button, Card, Col, Image, ListGroup, Row } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import { useEffect, useState } from 'react'
+import { useGetProductDetailsQuery } from '../slices/productsApiSlice'
 
 export default function ProductScreen() {
-	const [product, setProduct] = useState([])
-
 	const { id: productId } = useParams()
-
-	useEffect(() => {
-		const fetchProduct = async () => {
-			const { data } = await axios.get(`/api/products/${productId}`)
-			setProduct(data)
-		}
-
-		fetchProduct()
-	}, [productId])
+	const {
+		data: product,
+		isLoading,
+		error,
+	} = useGetProductDetailsQuery(productId)
 
 	return (
 		<>
-			<Link className='btn btn-light my-3' to='/'>
-				Go Back
-			</Link>
-			<Row>
-				<Col md={5}>
-					<Image src={product.image} alt={product.name} fluid />
-				</Col>
-				<Col md={4}>
-					<ListGroup>
-						<ListGroup.Item>
-							<h3>{product.name}</h3>
-						</ListGroup.Item>
-						<ListGroup.Item>
-							<Rating
-								value={product.rating}
-								text={`${product.numReviews} reviews`}
-							/>
-						</ListGroup.Item>
-						<ListGroup.Item>Description: {product.description}</ListGroup.Item>
-					</ListGroup>
-				</Col>
-				<Col md={3}>
-					<Card>
-						<ListGroup>
-							<ListGroup.Item>
-								<Row>
-									<Col>Price:</Col>
-									<Col>
-										<strong>${product.price}</strong>
-									</Col>
-								</Row>
-							</ListGroup.Item>
-							<ListGroup.Item>
-								<Row>
-									<Col>Status:</Col>
-									<Col>
-										<strong>
-											{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
-										</strong>
-									</Col>
-								</Row>
-							</ListGroup.Item>
-							<ListGroup.Item>
-								<Button
-									className='btn-block'
-									disabled={product.countInStock === 0}>
-									Add to Cart
-								</Button>
-							</ListGroup.Item>
-						</ListGroup>
-					</Card>
-				</Col>
-			</Row>
+			{isLoading ? (
+				<h2>Loading...</h2>
+			) : error ? (
+				<h2>{error?.data?.message || error.error}</h2>
+			) : (
+				<>
+					<Link className='btn btn-light my-3' to='/'>
+						Go Back
+					</Link>
+					<Row>
+						<Col md={5}>
+							<Image src={product.image} alt={product.name} fluid />
+						</Col>
+						<Col md={4}>
+							<ListGroup>
+								<ListGroup.Item>
+									<h3>{product.name}</h3>
+								</ListGroup.Item>
+								<ListGroup.Item>
+									<Rating
+										value={product.rating}
+										text={`${product.numReviews} reviews`}
+									/>
+								</ListGroup.Item>
+								<ListGroup.Item>
+									Description: {product.description}
+								</ListGroup.Item>
+							</ListGroup>
+						</Col>
+						<Col md={3}>
+							<Card>
+								<ListGroup>
+									<ListGroup.Item>
+										<Row>
+											<Col>Price:</Col>
+											<Col>
+												<strong>${product.price}</strong>
+											</Col>
+										</Row>
+									</ListGroup.Item>
+									<ListGroup.Item>
+										<Row>
+											<Col>Status:</Col>
+											<Col>
+												<strong>
+													{product.countInStock > 0
+														? 'In Stock'
+														: 'Out of Stock'}
+												</strong>
+											</Col>
+										</Row>
+									</ListGroup.Item>
+									<ListGroup.Item>
+										<Button
+											className='btn-block'
+											disabled={product.countInStock === 0}>
+											Add to Cart
+										</Button>
+									</ListGroup.Item>
+								</ListGroup>
+							</Card>
+						</Col>
+					</Row>
+				</>
+			)}
 		</>
 	)
 }
