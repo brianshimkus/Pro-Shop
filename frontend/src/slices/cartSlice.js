1e51baa16621495d59c646e33c5e25c3dcec0ecf
Brian Shimkus <brianshimkus1@gmail.com>
2024-03-27 23:52:38 -0500

    Calculate price

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,5 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+export const addDecimals = (num) => {
+	return (Math.round(num * 100) / 100).toFixed(2)
+}
+
 const initialState = localStorage.getItem('cart')
 	? JSON.parse(localStorage.getItem('cart'))
 	: { cartItems: [] }
@@ -21,8 +25,33 @@ const cartSlice = createSlice({
 			} else {
 				state.cartItems = [...state.cartItems, item]
 			}
+
+			const itemsPrice = state.cartItems.reduce(
+				(acc, item) => acc + (item.price * 100 * item.qty) / 100,
+				0
+			)
+			state.itemsPrice = addDecimals(itemsPrice)
+
+			// Calculate the shipping price
+			const shippingPrice = itemsPrice > 100 ? 0 : 10
+			state.shippingPrice = addDecimals(shippingPrice)
+
+			// Calculate the tax price
+			const taxPrice = 0.15 * itemsPrice
+			state.taxPrice = addDecimals(taxPrice)
+
+			const totalPrice = itemsPrice + shippingPrice + taxPrice
+			// Calculate the total price
+			state.totalPrice = addDecimals(totalPrice)
+
+			// Save the cart to localStorage
+			localStorage.setItem('cart', JSON.stringify(state))
+
+			return state
 		},
 	},
 })
+
+export const { addToCart } = cartSlice.actions
 
 export default cartSlice.reducer
