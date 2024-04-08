b734388560fa65f852eaf4bb1feb5817a713cf4e
Brian Shimkus <brianshimkus1@gmail.com>
2024-04-07 23:52:31 -0500

    Shipping page functionality

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -1,16 +1,26 @@
 import { useState } from 'react'
 import { Form, Button } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 import FormContainer from '../components/FormContainer'
+import { saveShippingAddress } from '../slices/cartSlice'
 
 export default function ShippingScreen() {
-	const [address, setAddress] = useState('')
-	const [city, setCity] = useState('')
-	const [postalCode, setPostalCode] = useState('')
-	const [country, setCountry] = useState('')
+	const cart = useSelector((state) => state.cart)
+	const { shippingAddress } = cart
+
+	const [address, setAddress] = useState(shippingAddress.address || '')
+	const [city, setCity] = useState(shippingAddress.city || '')
+	const [postalCode, setPostalCode] = useState(shippingAddress.postalCode || '')
+	const [country, setCountry] = useState(shippingAddress.country || '')
+
+	const dispatch = useDispatch()
+	const navigate = useNavigate()
 
 	const submitHandler = (e) => {
 		e.preventDefault()
-		console.log('submit')
+		dispatch(saveShippingAddress({ address, city, postalCode, country }))
+		navigate('/payment')
 	}
 
 	return (
