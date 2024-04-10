17bdc78d9bb97020276a7670a322623ec22e45f6
Brian Shimkus <brianshimkus1@gmail.com>
2024-04-10 01:22:27 -0500

    Get orders functionality

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -43,14 +43,25 @@ const addOrderItems = asyncHandler(async (req, res) => {
 // @route   GET /api/orders/myorders
 // @access  Private
 const getMyOrders = asyncHandler(async (req, res) => {
-	res.send('get my orders')
+	const orders = await Order.find({ user: req.user._id })
+	res.status(200).json(orders)
 })
 
 // @desc    Get order by ID
 // @route   GET /api/orders/:id
 // @access  Private
 const getOrderById = asyncHandler(async (req, res) => {
-	res.send('get order by ID')
+	const order = await Order.findById(req.params.id).populate(
+		'user',
+		'name email'
+	)
+
+	if (order) {
+		res.json(order)
+	} else {
+		res.status(404)
+		throw new Error('Order not found')
+	}
 })
 
 // @desc    Update order to paid
