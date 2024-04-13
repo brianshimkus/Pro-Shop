9f10b4c71a2556612a19c98dfe25063217b5e349
Brian Shimkus <brianshimkus1@gmail.com>
2024-04-12 23:58:44 -0500

    Setup PayPal script provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -21,6 +21,7 @@ import PrivateRoute from './components/PrivateRoute'
 import PaymentScreen from './screens/PaymentScreen'
 import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
+import { PayPalScriptProvider } from '@paypal/react-paypal-js'
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
@@ -45,7 +46,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<RouterProvider router={router} />
+			<PayPalScriptProvider deferLoading={true}>
+				<RouterProvider router={router} />
+			</PayPalScriptProvider>
 		</Provider>
 	</React.StrictMode>
 )
