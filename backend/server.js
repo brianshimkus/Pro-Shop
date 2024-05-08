edbb5b219013b7fcd9f595243378419721b9ef8b
Brian Shimkus <brianshimkus1@gmail.com>
2024-05-08 15:07:52 -0500

    Server update

--- backend/server.js
+++ backend/server.js
@@ -18,16 +18,9 @@ connectDB()
 
 const app = express()
 
-// Body parser middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
-// Cookie parser middleware
 app.use(cookieParser())
-
-app.get('/', (req, res) => {
-	res.send('API is running...')
-})
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
@@ -37,9 +30,6 @@ app.use('/api/upload', uploadRoutes)
 app.get('/api/config/paypal', (req, res) =>
 	res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 )
-
-const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 if (process.env.NODE_ENV === 'production') {
 	const __dirname = path.resolve()
@@ -60,6 +50,8 @@ if (process.env.NODE_ENV === 'production') {
 app.use(notFound)
 app.use(errorHandler)
 
-app.listen(port, () => {
-	console.log(`Server is running on port ${port}`.yellow)
-})
+app.listen(port, () =>
+	console.log(
+		`Server running in ${process.env.NODE_ENV} mode on port ${port}`.yellow
+	)
+)
